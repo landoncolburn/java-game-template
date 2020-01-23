bb219a71d66ebbf2a833e346d12a4455c65b565c
Landon Colburn <colburnlandon@gmail.com>
2020-01-23 01:03:24 -0600

    Fixed splashscreen

--- Splashscreen.java
+++ Splashscreen.java
@@ -4,19 +4,38 @@ import java.awt.image.*;
 public class Splashscreen extends GameObject{
 
   private BufferedImage[] studioLogo = new BufferedImage[10];
+  private BufferedImage activeLogo = null;
+  int count = 0;
 
   public Splashscreen(){
     super(0, 0);
     for(int i = 0; i<10; i++){
-      studioLogo[i] = Game.gameInstance.bil.loadImage("assets/studio/GameDesignLogo"+i);
+      studioLogo[i] = Game.gameInstance.bil.loadImage("assets/studio/GameDesignLogo"+i+".png");
     }
   }
 
-  public void tick(){}
+  public void tick(){
+    if(count<245){
+      count++;
+    }
+    if(count < 44){
+      activeLogo = studioLogo[9-(int)((count)/5)];
+    }
+    else if(count > 44 && count < 200){
+      activeLogo = studioLogo[0];
+    }
+    else if(count>200 && count < 244){
+      activeLogo = studioLogo[(int)((count-200)/5)];
+    }
+    else if(count>244){
+      activeLogo = null;
+    }
+  }
+
   public Rectangle getBounds(){return null;}
 
   public void render(Graphics g){
-    g.drawImage(studioLogo[0], Game.gameInstance.size.width/2-150, Game.gameInstance.size.height/2-45, null);
+    g.drawImage(activeLogo, Game.gameInstance.size.width/2-150, Game.gameInstance.size.height/2-45, null);
   }
 
 }
